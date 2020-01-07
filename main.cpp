420ad0ae6420a70a331b6eef90497f5743ccc162
Jeff Hutchison <jeff@jeffhutchison.com>
2020-01-07 18:13:37 -0500

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,3 @@
-#include <spdlog/spdlog.h>
+#include "deadeye.h"
 
-#include "deadeye.hpp"
-#include "pipeline.hpp"
-
-using namespace deadeye;
-using namespace ded;
-
-namespace {
-void ConfigureLogging() {
-  spdlog::set_level(spdlog::level::debug);
-  spdlog::set_pattern("[%H:%M:%S.%e] [%t] [%^%l%$]: %v");
-}
-}  // namespace
-
-int main(int argc, char** argv) {
-  ConfigureLogging();
-  spdlog::info("Deadeye unit {} starting", DEADEYE_UNIT);
-
-  try {
-    DEADEYE_INIT()
-    Controller::GetInstance().Run();
-  } catch (std::exception const& e) {
-    spdlog::critical("Controller::Run exception: {}", e.what());
-    std::exit(EXIT_FAILURE);
-  }
-  std::exit(EXIT_SUCCESS);
-}
+DEADEYE_MAIN()
